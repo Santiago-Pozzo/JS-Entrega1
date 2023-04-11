7038844d1c24159033806bf4446bcb4d30de1880
Santiago-Pozzo <santiagopozzo@gmail.com>
2023-04-10 22:36:29 -0300

    Funciones 6, 7 y 8

--- main.js
+++ main.js
@@ -23,27 +23,25 @@
      }
  }
 
- alert("Ingrese un número para saber si es par o impar");
- parImpar(getNumber());
+//  alert("Ingrese un número para saber si es par o impar");
+//  parImpar(getNumber());
 
 
 //2)Comparar números
  const comparar = (number1, number2) => {
      if (number1 === number2) {
-         console.log(number1 + " Es igual a "+ number2);
+        console.log(number1 + " Es igual a "+ number2);
+     }    
+     if(number1 > number2) {
+        console.log(number1 + " Es mayor a " + number2);
      }
      else {
-         if(number1 > number2) {
-             console.log(number1 + " Es mayor a " + number2);
-         }
-         else {
-             console.log(number1 + " Es menor que " + number2);
-         }
+        console.log(number1 + " Es menor que " + number2);
      }
  }
 
- alert("Ingrese dos números para saber cuál es mayor"); 
- comparar(getNumber(), getNumber());
+//  alert("Ingrese dos números para saber cuál es mayor"); 
+//  comparar(getNumber(), getNumber());
 
 
 //3)Múltiplo de 5
@@ -57,8 +55,8 @@
     }
  }
 
- alert("Ingrese un número para saber si es múltiplo de 5");
- multiploDeCinco(getNumber());
+//  alert("Ingrese un número para saber si es múltiplo de 5");
+//  multiploDeCinco(getNumber());
 
 
 //4)Contar hasta n
@@ -68,8 +66,8 @@
     }
  }
 
- alert("Ingrese un número para vizualizar todos los números desde 0 hasta el número ingresado");
- contarHasta(getNumber());
+//  alert("Ingrese un número para vizualizar todos los números desde 0 hasta el número ingresado");
+//  contarHasta(getNumber());
 
 
 //5)Imprimir palabra n veces
@@ -79,5 +77,31 @@
     }
  }
  
- alert("Ingrese una palabra y un numero para ver esa palabra repetida en la cantidad ingresada");
- imprimirPalabraNVeces(prompt("Ingrese una palabra"),getNumber());
+//  alert("Ingrese una palabra y un numero para ver esa palabra repetida en la cantidad ingresada");
+//  imprimirPalabraNVeces(prompt("Ingrese una palabra"),getNumber());
+
+//6) Imprimir valores de un arreglo
+ const imprimirArray = (array) => {
+    for (let index = 0; index < array.length; index++) {
+        console.log(array[index]);        
+    }
+ }
+
+//  imprimirArray (["hola", "esto", "es", "un", "array", "muy", "variado", 1, 3, null, "pancho con papitas", true])
+
+//7) Imprimir array de 10 números menos el 5to elemento 
+ const imprimirArraySinElQuinto = (array) => {
+    for (let index = 0; index < 10; index++) {
+        if (index===4) continue;             
+        else console.log(array[index]);        
+    }
+ }
+//  imprimirArraySinElQuinto([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+//8) Imprimir elementos del array multiplicados por n
+ const elementosMultiplicados = (array, n) => {
+    for (let index = 0; index < array.length; index++) {
+        console.log(array[index]*n);
+    }    
+ }
+//  elementosMultiplicados ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], getNumber());
